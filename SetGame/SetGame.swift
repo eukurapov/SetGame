ef150905ac5c4a81152dccb10472ceb54f4883e5
eukurapov <eukurapov@gmail.com>
2020-06-16 18:51:28 +0300

    Fix for cards added not by order

--- SetGame/SetGame.swift
+++ SetGame/SetGame.swift
@@ -32,17 +32,14 @@ struct SetGameModel<CardContent> where CardContent: Matchable {
             cards.append(Card(id: index, content: cardContentFactory(index)))
         }
         cards.shuffle()
-        print(cards.prefix(20).map{ $0.id })
         deal(numberOfCardsToStart)
     }
     
     mutating func deal(_ numberToDeal: Int = 1) {
-        print(deck.prefix(20).map{ $0.id })
         for _ in 0..<numberToDeal {
             if !deck.isEmpty {
                 if let index = cards.firstIndexOf(deck[0]) {
                     cards[index].isDealt = true
-                    print("card \(cards[index].id), index \(index)")
                 }
             }
         }
